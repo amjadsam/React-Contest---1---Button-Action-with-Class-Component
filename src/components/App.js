7be80504ec33f3845737b19a6f57923458cfcad8
amjadsam <68984249+amjadsam@users.noreply.github.com>
2021-01-19 16:36:41 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,20 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state={
+			count:0
+		};
 	};
-
+	clickFun=()=>{
+     this.setState({count:this.state.count+1});
+	}
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
-    		</div>
+    		<button id="click" onClick={this.clickFun} >click</button>
+			{this.state.count===1?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null}
+			</div>
     	);
     }
 }
